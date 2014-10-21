fa458a85ad88af873b7a36b6ce584a66d8009259
PeteM <github@thrap.co.uk>
2014-10-21 15:58:50 +0100

    advert and boxes on home page

--- page-templates/front-page.php
+++ page-templates/front-page.php
@@ -20,13 +20,129 @@
                 
                 <?php reactor_inner_content_before(); ?>
                 
-					<?php the_content(); ?>
-                    
-                <?php reactor_inner_content_after(); ?>
+					<?php // the_content(); ?>
+                    <?php reactor_inner_content_after(); ?>
+ 
                 
+
+                    
+<!---------------------- MOVE THIS TO AN INC ------------------------------------------------------->
+   
+            <h1>Creative teaching and training resources for the EYFS</h1>
+            <ul class="small-block-grid-2 large-block-grid-4 photo-blocks">        
+                <li>
+                    <a href="/topic/natural-recycled-materials/" class="photo-box-link">
+                    <div class="photo-box natural">
+                    <img src="/wp-content/themes/earlyarts/images/home-categories/natural1.jpg">          
+                    <div class="photo-box-caption">
+                    Natural &amp; Recycled    
+                    </div>  
+                    </div>
+                    </a>    
+                </li>
+                <li>
+                    <a href="/topic/music-singing/" class="photo-box-link">
+                    <div class="photo-box">
+                    <img src="/wp-content/themes/earlyarts/images/home-categories/music1.jpg">    
+                    <div class="photo-box-caption">
+                    Music &amp; Singing    
+                    </div>
+                    </div>
+                    </a>  
+                </li>
+                <li>
+                    <a href="/topic/crafts-construction/" class="photo-box-link">
+                    <div class="photo-box">
+                    <img src="/wp-content/themes/earlyarts/images/home-categories/crafts1.jpg">
+                    <div class="photo-box-caption">
+                    Crafts & Construction   
+                    </div>                     
+                    </div>   
+                    </a>  
+                </li>
+                <li>
+                    <a href="/topic/photography-video/" class="photo-box-link">
+                    <div class="photo-box">
+                    <img src="/wp-content/themes/earlyarts/images/home-categories/photography1.jpg">
+                    <div class="photo-box-caption">
+                     Video & Photography  
+                    </div>                     
+                    </div>      
+                    </a>  
+                </li>
+            </ul> 
+    
+           <ul class="small-block-grid-2 large-block-grid-4">        
+                <li>
+                    <a href="/topic/drawing-writing-mark-making/" class="photo-box-link">
+                    <div class="photo-box">
+                    <img src="/wp-content/themes/earlyarts/images/home-categories/drawing1.jpg"> 
+                    <div class="photo-box-caption">
+                     Drawing  & Mark Making 
+                    </div>                     
+                    </div>    
+                    </a>  
+                </li>
+                <li>
+                    <a href="/topic/storytelling-puppets-roleplay/" class="photo-box-link">
+                    <div class="photo-box">
+                    <img src="/wp-content/themes/earlyarts/images/home-categories/puppets1.jpg">
+                    <div class="photo-box-caption">
+                      Puppets & Story Telling   
+                    </div>                     
+                    </div>   
+                    </a>  
+                </li>
+                <li>
+                    <a href="/topic/movement-dance/" class="photo-box-link">
+                    <div class="photo-box">
+                   <img src="http://lorempixel.com/349/209/abstract/8">
+                    <div class="photo-box-caption">
+                     Dance & Movement    
+                    </div>                     
+                    </div>     
+                    </a>  
+                </li>
+                <li>
+                    <a href="#" class="photo-box-link">
+                    <div class="photo-box">
+                  <img src="http://lorempixel.com/349/209/abstract/10">
+                    <div class="photo-box-caption">
+                    SPECIAL OFFER
+                    </div>                     
+                    </div>   
+                    </a>  
+                </li>
+            </ul>                  
+            
+                    
+                    
+                    
+<!---------------------- MOVE THIS TO AN INC ------------------------------------------------------->                    
                 </div><!-- .columns -->
-            </div><!-- .row -->
-            <div class="row">     
+            </div><!-- .row -->         
+
+            <div class="row">
+                <ul class="small-block-grid-1 large-block-grid-2 content-foot-block">
+                    <li><h2>Join us for a 10% Discount</h2>
+                    <p>As an Earlyarts member, you'll receive
+                    </p>
+                    <ul>
+                    <li>10% off all products</li>
+                    <li>Free training webinars</li>
+                    <li>Exclusive teaching pack extracts</li></ul>
+                    <p><a class="radius success button" href="/subscribe-to-earlyarts/">Join us</a></p>
+                    </li>
+                    <li>
+                    <h2>Free Early Years Bulletin</h2>
+                    <p>Every month we choose you the best and most relevant links</p>
+                        <ul>
+                        <li>Training opportunities</li>
+                        <li>Relevant resources</li>
+                        <li>New research</li>
+                        </ul>
+                    <p><a class="radius success button" href="/subscribe-e-bulletin/">Get the e-bulletin</a></p></li>
+                </ul> 
             <?php get_sidebar('frontpage'); ?>
             </div><!-- .row -->            
         </div><!-- #content -->
--- sidebar-frontpage.php
+++ sidebar-frontpage.php
@@ -13,21 +13,9 @@
     reactor_sidebar_before();
     if ( is_active_sidebar('sidebar-frontpage')) : 
 ?>
-        <div id="sidebar-frontpage" class="sidebar <?php reactor_columns( 6 ); ?>" role="complementary">
+        <div id="sidebar-frontpage" class="sidebar <?php reactor_columns( 12 ); ?>" role="complementary">
             <?php dynamic_sidebar('sidebar-frontpage'); ?>
         </div><!-- #sidebar-frontpage -->
     <?php endif; ?>
 
-    <?php reactor_sidebar_after(); ?>
-    
-
-    <?php 
-    reactor_sidebar_before();    
-    if ( is_active_sidebar('sidebar-frontpage-2')) : 
-     ?>
-    
-        <div id="sidebar-frontpage-2" class="sidebar <?php reactor_columns( 6 ); ?>" role="complementary">
-            <?php dynamic_sidebar('sidebar-frontpage-2'); ?>
-        </div><!-- #sidebar-frontpage-2 -->    
-    <?php endif; ?>
     <?php reactor_sidebar_after(); ?>
