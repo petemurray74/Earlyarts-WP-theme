05755980583170c28e9fe08db1329522605a4a49
PeteM <github@thrap.co.uk>
2015-04-15 15:27:16 +0100

    change design of signup box

--- get-updates.php
+++ get-updates.php
@@ -1,10 +1,12 @@
-<div class="panel callout">
-<h3>Like this post? Get news and blogs delivered to your inbox</h3>
-                        <p>Subscribe to our monthly e-bulletin</p>
-                        
-<?php
-$widgetNL = new WYSIJA_NL_Widget(true);
-echo $widgetNL->widget(array('form' => 4, 'form_type' => 'php'));                     
-?>
-
+<div class="panel">
+<h2>Like this post?<br>Get opportunities and blogs delivered to your inbox</h2>
+<p>Subscribe to our monthly early years e-bulletin</p>
+        <div class="row" style="background-color: #F2F2F2;">
+        <div class="large-6 column">                        
+            <?php
+            $widgetNL = new WYSIJA_NL_Widget(true);
+            echo $widgetNL->widget(array('form' => 4, 'form_type' => 'php'));                     
+            ?>
+        </div>
+        </div>
 </div>
