d87c61af7358d5e33f0bca2478dd43f66a7a29a9
PeteM <github@thrap.co.uk>
2015-05-13 15:03:56 +0100

    new front page template

---
+++ page-templates/front-page-landing-page.php
@@ -0,0 +1,107 @@
+<?php
+/**
+ * Template Name: Front Page Landing Page 
+ *
+ * @package Reactor
+ * @subpackge Page-Templates
+ * @since 1.0.0
+ */
+?>
+<?php get_template_part('page-templates/meta-controls');?>
+<?php get_header(); ?>
+
+	<div id="primary" class="landing-page1">
+    
+    	<?php //reactor_content_before(); ?>
+   
+        <div id="content" role="main">
+                
+                <?php reactor_inner_content_before(); ?>
+                
+					<?php reactor_page_before(); ?>
+					
+					<article id="post-<?php echo $page_id; ?>" class="<?php echo $class; ?>">   
+																
+						<div class="entry-content">
+							<?php the_content(); ?>
+						</div>
+					</article><!-- #post -->
+					
+					<?php reactor_page_after(); ?>
+                    
+                <?php reactor_inner_content_after(); ?>
+                
+
+        </div><!-- #content -->
+        
+        <?php reactor_content_after(); ?>
+        
+<?php 
+if (get_post_meta($post->ID,'include_sidebar',true)=='yes')
+    {
+    if (is_active_sidebar('frontpage')) : 
+    ?>
+    <div class="row">
+    <div id="secondary" class="large-12 columns frontpage-sidebar">
+    <?php
+    dynamic_sidebar('frontpage'); 
+    endif;
+    ?>
+    </div></div>  
+        </div><!-- #primary -->
+    <?php
+    }
+
+// this Javascript takes variable from a query string and adds them to a form as hidden fields
+// from http://www.terminusapp.com/blog/bet-you-havent-used-utm-parameters-like-this
+// use 'utm_' fields for external links, so they also show on Analytics
+// use 'source' for internal
+
+if (get_post_meta($post->ID,'include_tracking_code',true)=='yes')
+{
+?>
+<script type="text/javascript">
+  function setupUtmParamForm() {
+    function getParameterByName(name) {
+      name = name.replace(/[\[]/, "\\\[").replace(/[\]]/, "\\\]");
+      var regexS = "[\\?&#038;]" + name + "=([^&#]*)";
+      var regex = new RegExp(regexS);
+      var results = regex.exec(window.location.search);
+      if(results == null) {
+        return "";
+      } else {
+        return decodeURIComponent(results[1].replace(/\+/g, " "));
+      }
+    }
+    jQuery(document).ready(function () {
+      function addFormElem(paramName, fieldName) {
+        var paramValue = getParameterByName(paramName);
+        var $utmEl = jQuery("<input type='hidden' name='" + fieldName + "' value='" + paramValue + "'>");
+        if (paramValue != "") {
+          jQuery("form").first().prepend($utmEl);
+        }
+      }
+ 
+      var utmParams = {
+        "utm_source"   : "USOURCE",
+        "utm_medium"   : "UMEDIUM",
+        "utm_campaign" : "UCAMPAIGN",
+        "source"       : "MMERGE3"
+      };
+ 
+      for (var param in utmParams) {
+        addFormElem(param, utmParams[param]);
+      }
+    });
+  }
+setupUtmParamForm();
+</script>
+<?php    
+} // end mailchimp tracking code
+get_footer();
+?>
+
+        
+	
+
+
