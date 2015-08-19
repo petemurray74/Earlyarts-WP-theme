617d0703fe220c81e262245756d08d4c38a737bb
petemurray74 <github@thrap.co.uk>
2015-08-19 12:05:17 +0100

    Moved from MailPoet to MailChimp

--- get-updates.php
+++ get-updates.php
@@ -3,10 +3,16 @@
 <p>Subscribe to our monthly early years e-bulletin</p>
         <div class="row" style="background-color: #F2F2F2;">
         <div class="large-6 column">                        
-            <?php
-            $widgetNL = new WYSIJA_NL_Widget(true);
-            echo $widgetNL->widget(array('form' => 4, 'form_type' => 'php'));                     
-            ?>
+<form action="//earlyarts.us7.list-manage.com/subscribe/post?u=12b8cbd1895de4e1c60f826e9&amp;id=42ae85e286" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate><input id="mce-REQUESTED" class="" name="REQUESTED" type="hidden" value="ebulletin" /><input id="mce-MMERGE3" class="" name="MMERGE3" type="hidden" value="blog-footer" /><input type="hidden" value="1" name="group[11609][1]" id="mce-group[11609]-11609-0">
+<div id="mc_embed_signup_scroll">
+<div class="mc-field-group" style="margin-top: 5px; clear: both;"><label for="mce-FNAME">First Name </label><input id="mce-FNAME" class="" style="cursor: auto;" name="FNAME" type="text" value="" /></div>
+<div class="mc-field-group"><label for="mce-EMAIL">Email Address <span class="asterisk">*</span>
+</label><input id="mce-EMAIL" class="required email" name="EMAIL" type="email" value="" /></div>
+<div id="mce-responses" class="clear"></div>
+<div style="position: absolute; left: -5000px;"><!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups--><input tabindex="-1" name="b_12b8cbd1895de4e1c60f826e9_42ae85e286" type="text" value="" /></div>
+<div id="free-sample-download" class="clear"><input id="mc-embedded-subscribe" class="button medium success radius" style="font-size: 16px; margin: 10px 0 0 0;" name="subscribe" type="submit" value="Get updates" /></div>
+</div>
+</form>
         </div>
         </div>
 </div>
