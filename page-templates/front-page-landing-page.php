b1cb6ffce5564758563677e82dccb094c13d07b3
petemurray74 <github@thrap.co.uk>
2015-10-14 11:32:58 +0100

    Remove tracking code which is now added via tagmanager

--- page-templates/front-page-landing-page.php
+++ page-templates/front-page-landing-page.php
@@ -47,55 +47,9 @@
     ?>
     </div></div>  
        
-    <?php
 
-// this Javascript takes variable from a query string and adds them to a form as hidden fields
-// from http://www.terminusapp.com/blog/bet-you-havent-used-utm-parameters-like-this
-// use 'utm_' fields for external links, so they also show on Analytics
-// use 'source' for internal
-
-if (get_post_meta($post->ID,'include_tracking_code',true)=='yes')
-{
-?>
-<script type="text/javascript">
-  function setupUtmParamForm() {
-    function getParameterByName(name) {
-      name = name.replace(/[\[]/, "\\\[").replace(/[\]]/, "\\\]");
-      var regexS = "[\\?&#038;]" + name + "=([^&#]*)";
-      var regex = new RegExp(regexS);
-      var results = regex.exec(window.location.search);
-      if(results == null) {
-        return "";
-      } else {
-        return decodeURIComponent(results[1].replace(/\+/g, " "));
-      }
-    }
-    jQuery(document).ready(function () {
-      function addFormElem(paramName, fieldName) {
-        var paramValue = getParameterByName(paramName);
-        var $utmEl = jQuery("<input type='hidden' name='" + fieldName + "' value='" + paramValue + "'>");
-        if (paramValue != "") {
-          jQuery("form").first().prepend($utmEl);
-        }
-      }
- 
-      var utmParams = {
-        "utm_source"   : "USOURCE",
-        "utm_medium"   : "UMEDIUM",
-        "utm_campaign" : "UCAMPAIGN",
-        "source"       : "MMERGE3"
-      };
- 
-      for (var param in utmParams) {
-        addFormElem(param, utmParams[param]);
-      }
-    });
-  }
-setupUtmParamForm();
-</script>
-</div><!-- #primary -->
+	</div><!-- #primary -->
 <?php    
-} // end mailchimp tracking code
 get_footer();
 ?>
 
