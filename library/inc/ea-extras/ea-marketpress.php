02748ef6b9f51ec1003cc2c8cfd31c7f55bdf955
petemurray74 <github@thrap.co.uk>
2015-12-23 21:38:12 +0000

    remove marketpress v2x functions

--- library/inc/ea-extras/ea-marketpress.php
+++ library/inc/ea-extras/ea-marketpress.php
@@ -1,108 +1,4 @@
 <?php
-/*
-function ea_mp_list_products($echo = true, $paginate = '', $page = '', $per_page = '', $order_by = '', $order = '', $category = '', $tag = '', $list_view = NULL) {
-		global $wp_query, $mp;
-		//setup taxonomy if applicable
-		if ($category) {
-			$taxonomy_query = '&product_category=' . sanitize_title($category);
-		} else if ($tag) {
-			$taxonomy_query = '&product_tag=' . sanitize_title($tag);
-		} else if (isset($wp_query->query_vars['taxonomy']) && ($wp_query->query_vars['taxonomy'] == 'product_category' || $wp_query->query_vars['taxonomy'] == 'product_tag')) {
-			//TODO might need to fix for tags that are a number
-			$taxonomy_query = '&' . $wp_query->query_vars['taxonomy'] . '=' . $wp_query->query_vars['term'];
-		} else {
-			$taxonomy_query = '';
-		}
-
-		//setup pagination
-		$paged = false;
-		if ($paginate) {
-			$paged = true;
-		} else if ($paginate === '') {
-			if ($mp->get_setting('paginate'))
-				$paged = true;
-			else
-				$paginate_query = '&nopaging=true';
-		} else {
-			$paginate_query = '&nopaging=true';
-		}
-
-		//get page details
-		if ($paged) {
-			//figure out perpage
-			if (intval($per_page)) {
-				$paginate_query = '&posts_per_page=' . intval($per_page);
-			} else {
-				$paginate_query = '&posts_per_page=' . $mp->get_setting('per_page');
-			}
-
-			//figure out page
-			if (isset($wp_query->query_vars['paged']) && $wp_query->query_vars['paged'])
-				$paginate_query .= '&paged=' . intval($wp_query->query_vars['paged']);
-
-			if (intval($page))
-				$paginate_query .= '&paged=' . intval($page);
-			else if ($wp_query->query_vars['paged'])
-				$paginate_query .= '&paged=' . intval($wp_query->query_vars['paged']);
-		}
-
-		//get order by
-		if (!$order_by) {
-			if ($mp->get_setting('order_by') == 'price')
-				$order_by_query = '&meta_key=mp_price_sort&orderby=meta_value_num';
-			else if ($mp->get_setting('order_by') == 'sales')
-				$order_by_query = '&meta_key=mp_sales_count&orderby=meta_value_num';
-			else
-				$order_by_query = '&orderby=' . $mp->get_setting('order_by');
-		} else {
-			if ('price' == $order_by)
-				$order_by_query = '&meta_key=mp_price_sort&orderby=meta_value_num';
-			else if ('sales' == $order_by)
-				$order_by_query = '&meta_key=mp_sales_count&orderby=meta_value_num';
-			else
-				$order_by_query = '&orderby=' . $order_by;
-		}
-
-		//get order direction
-		if (!$order) {
-			$order_query = '&order=' . $mp->get_setting('order');
-		} else {
-			$order_query = '&order=' . $order;
-		}
-
-		//The Query
-		$custom_query = new WP_Query('post_type=product&post_status=publish' . $taxonomy_query . $paginate_query . $order_by_query . $order_query);
-
-		//allows pagination links to work get_posts_nav_link()
-		if ($wp_query->max_num_pages == 0 || $taxonomy_query)
-			$wp_query->max_num_pages = $custom_query->max_num_pages;
-
-		// get layout type for products
-		if (is_null($list_view)) {
-			$layout_type = $mp->get_setting('list_view');
-		} else {
-			$layout_type = $list_view ? 'list' : 'grid';
-		}
-
-		$content = '<ul class="small-block-grid-2 large-block-grid-3">';
-		
-		if ($last = $custom_query->post_count) {
-
-			$content .= $layout_type == 'grid' ?
-					ea_mp_products_html_grid($custom_query->posts) :
-					_mp_products_html_list($custom_query->posts);
-		} else {
-			$content .= '<div id="mp_no_products">' . apply_filters('mp_product_list_none', __('No Products', 'mp')) . '</div>';
-		}
-
-		$content .= '</ul>';
-		
-		if ($echo)
-			echo $content;
-		else
-			return $content;
-}
-*/
 
 // overwriting the same function from template-functions.php
 // hacked to work for grid display only
@@ -150,7 +46,7 @@ function ea_mp_list_products($echo = true, $paginate = '', $page = '', $per_page
 
 			$html .= '
 				<li><div class="ea_mp_product_detail">
-					<div itemscope itemtype="http://schema.org/Product" class="mp_product' . ( ( strlen( $img ) > 0 ) ? ' mp_product-has-image' . $align_class : '' ) . ' ' . implode( $class, ' ' ) . '">
+					<div itemscope itemtype="http://schema.org/Product" class="mp_product' . ( ( strlen( $img ) > 0 ) ? ' mp_product-has-image' : '' ) . ' ' . implode( $class, ' ' ) . '">
 					
 						<div class="mp_product_images">
 							' . $img . '
@@ -232,25 +128,6 @@ function ea_mp_list_products($echo = true, $paginate = '', $page = '', $per_page
     return $html;
 }
 */
-
-
-
-// GIVE 10% discount to members - currently using a coupon code membership add-on rather than this
-function ea_members_discount($price) {
-// check if they are a member (can add more membership ids separated by commas  if you want to check whether they are a member of any of a few plans)
-// function_exists('ms_has_membership') check to see if the plugin exists
-if ( function_exists('ms_has_membership') && ms_has_membership(7675) ) {
-	$price['lowest'] = $price['lowest']*0.9;
-	//if there is a sale on, discount that also
-	if ($price['sale']['amount']) 
-	{
-	$price['sale']['amount'] = $price['sale']['amount']*0.9;
-	}
-  }
-return $price;
-}
-//add_filter( 'mp_product/get_price', 'ea_members_discount' , 10, 2 );
-
 
 
 // shortcode to add an 'add to cart' button to a page
